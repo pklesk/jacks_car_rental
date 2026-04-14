a52ea50b3276c6715eedf9ea81b54bcee9385dff
pklesk <pklesk@wi.zut.edu.pl>
2026-04-14 21:53:35 +0200

    experiments from 3090

--- src/jcr.py
+++ src/jcr.py
@@ -17,8 +17,8 @@ from matplotlib import pyplot as plt
 from scipy.ndimage import label
 
 # global settings
-MAX_CARS_AT_LOC = 10
-MAX_CARS_MOVED = 5
+MAX_CARS_AT_LOC = 20
+MAX_CARS_MOVED = 10
 REWARD_CAR_RENTED = np.float32(10.0)
 REWARD_CAR_MOVED = np.float32(-2.0)
 REQUEST_POISSON_LAMBDAS_AT_LOC = [3.0, 4.0]
