d454e629e4afa2ab3732a71ded9ca3984066c37e
pklesk <pklesk@wi.zut.edu.pl>
2026-04-23 19:20:58 +0200

    clean main (only cuda approaches on)

--- src/jcr.py
+++ src/jcr.py
@@ -17,7 +17,7 @@ from matplotlib import pyplot as plt
 from scipy.ndimage import label
 
 # global settings
-MAX_CARS_AT_LOC = 20
+MAX_CARS_AT_LOC = 10
 MAX_CARS_MOVED = 5
 REWARD_CAR_RENTED = np.float32(10.0)
 REWARD_CAR_MOVED = np.float32(-2.0)
